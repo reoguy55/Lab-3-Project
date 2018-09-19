d1a6350ad41176a6246c4ff295fb89f75ae51ec1
danielgleason <dlg2@illinois.edu>
2018-09-19 14:22:10 -0500

    Pre-Push

--- src/com/company/labApp.java
+++ src/com/company/labApp.java
@@ -2,30 +2,36 @@ package com.company;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-    public class labApp {
-        public static void main(String[] unused) {
-            System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
-        }
+public class labApp {
+    public static void main(String[] unused) {
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(wordCount(urlToString("http://erdani.com/tdpl/hamlet.txt")));
+    }
 
 
-        /**
-         * Retrieve contents from a URL and return them as a string.
-         *
-         * @param url url to retrieve contents from
-         * @return the contents from the url as a string, or an empty string on error
-         */
-        public static String urlToString(final String url) {
-            Scanner urlScanner;
-            try {
-                urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
-            } catch (IOException e) {
-                return "";
-            }
-            String contents = urlScanner.useDelimiter("\\A").next();
-            urlScanner.close();
-            return contents;
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    private static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return "";
         }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    private static int wordCount(String text) {
+        return text.split("\b").length;
+    }
 }
 
